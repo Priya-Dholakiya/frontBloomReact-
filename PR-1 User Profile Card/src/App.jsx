250082749bb0c52192752df86fdc73a5d2f5dc70
Priya-Dholakiya <priyadholakiya16@gmail.com>
2026-01-30 17:24:19 +0530

    React JS

--- PR-1 User Profile Card/src/App.jsx
+++ PR-1 User Profile Card/src/App.jsx
@@ -1,38 +1,64 @@
-import { useState, useEffect } from "react";
-import UserForm from "./Components/UserForm";
 import UserProfileCard from "./Components/UserProfileCard";
-import "./App.css";
 
 function App() {
-  const [ users, setUsers ] = useState( [] );
-
-  // 🔁 Reload pe data lao
-  useEffect( () => {
-    const savedUsers = localStorage.getItem( "userProfiles" );
-    if ( savedUsers ) {
-      setUsers( JSON.parse( savedUsers ) );
+  const users = [
+    {
+      name: "Sonalika Bhide",
+      email: "sonuu@test.in",
+      role: "Frontend Developer",
+      location: "Surat",
+      phone: "9876543210",
+      profilePicture:"https://tse4.mm.bing.net/th/id/OIP.2zEGWNBBts15ED4Lysj1QQHaEK?pid=Api&P=0&h=180"
+    },
+    {
+      name: "Jethalal Gada",
+      email: "jethalal@test.in",
+      role: "Backend Developer",
+      location: "Ahmedabad",
+      phone: "9123456789",
+      profilePicture:"https://tse1.mm.bing.net/th/id/OIP.LbSk68lzFev9N7XTtvhORgHaEK?pid=Api&P=0&h=180"
+    },
+    {
+      name: "Daya Gada",
+      email: "daya@test.in",
+      role: "UI Designer",
+      location: "Vadodara",
+      phone: "9012345678",
+      profilePicture:"https://tse4.mm.bing.net/th/id/OIP.g3nFQlYbeASLkfDtQAm5IwHaFj?pid=Api&P=0&h=180"
+    },
+    {
+      name: "Tipendra Gada",
+      email: "tapuu@test.in",
+      role: "Full Stack Developer",
+      location: "Rajkot",
+      phone: "9988776655",
+      profilePicture:"https://tse2.mm.bing.net/th/id/OIP.li-EWZNRax5TyHgHZf-D6QHaEK?pid=Api&P=0&h=180"
+    },
+    {
+      name: "Babita Iyer",
+      email: "babita@test.in",
+      role: "Full Stack Developer",
+      location: "Delhi",
+      phone: "9988776655",
+      profilePicture:"https://tse4.mm.bing.net/th/id/OIP.CsIzEq9SfnJif0xbIxy7vgHaHd?pid=Api&P=0&h=180"
     }
-  }, [] );
-
-  // ➕ New card add karo
-  const handleSubmit = ( data ) => {
-    const updatedUsers = [ ...users, data ];
-    setUsers( updatedUsers );
-    localStorage.setItem( "userProfiles", JSON.stringify( updatedUsers ) );
-  };
+  ];
 
   return (
-    <div className="page">
-      <UserForm onSubmit={handleSubmit} />
-
-      <div className="cards-grid">
-        {users.map( ( user, index ) => (
-          <UserProfileCard key={index} {...user} />
-        ) )}
-      </div>
+    <div className="container">
+      {/* 🔥 YAHIN component CALL ho raha hai */}
+      {users.map((user, index) => (
+        <UserProfileCard
+          key={index}
+          name={user.name}
+          email={user.email}
+          role={user.role}
+          location={user.location}
+          phone={user.phone}
+          profilePicture={user.profilePicture}
+        />
+      ))}
     </div>
   );
-
 }
-
 export default App;
--- PR-1 User Profile Card/src/Components/UserProfileCard.jsx
+++ PR-1 User Profile Card/src/Components/UserProfileCard.jsx
@@ -1,29 +1,28 @@
-function UserProfileCard( {
-      name,
-      email,
-      role,
-      location,
-      phone,
-      profilePicture
-} ) {
-      return (
-            <div className="card">
-                  <img
-                        src={profilePicture || "/avatar1.png"}
-                        className="avatar"
-                        alt="profile"
-                  />
-
-                  <h2>{name}</h2>
-                  <p className="email">{email}</p>
-
-                  <div className="details">
-                        <p><b>Role:</b> {role}</p>
-                        <p><b>Location:</b> {location}</p>
-                        <p><b>Phone:</b> {phone}</p>
-                  </div>
-            </div>
-      );
+function UserProfileCard({
+  name,
+  email,
+  role,
+  location,
+  phone,
+  profilePicture,
+}) {
+  return (
+    <div className="card">
+      <img src={profilePicture} className="avatar" alt="profile" />
+      <h2>{name}</h2>
+      <p className="email">{email}</p>
+      <div className="details">
+        <p>
+          <b>Role:</b> {role}
+        </p>
+        <p>
+          <b>Location:</b> {location}
+        </p>
+        <p>
+          <b>Phone:</b> {phone}
+        </p>
+      </div>
+    </div>
+  );
 }
-
 export default UserProfileCard;
